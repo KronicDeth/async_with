eda6580965e421ad003efe89df0a8c4946aff334
Luke Imhoff <Kronic.Deth@gmail.com>
2017-07-05 21:59:01 -0500

    Sort and privatize functions

--- lib/async_with.ex
+++ lib/async_with.ex
@@ -1,9 +1,63 @@
 defmodule AsyncWith do
-  defmacro async(expr, do: block) do
-    thing(expr, block)
+  # Macros
+
+  defmacro async(expr, do: block), do: do_async(expr, block)
+
+  # Functions
+
+  ## Private Functions
+
+  defp clean_vars(vars, replace \\ nil) do
+    IO.puts "clean_vars"
+    vars
+    |> Enum.map_reduce([], fn(var = {name, index}, acc) ->
+      case name in acc do
+        true -> {replace || {:_, index}, acc}
+        false -> {var, [name|acc]}
+      end
+    end)
+    |> elem(0)
   end
 
-  def thing({:with, _, withs}, block) do
+  defp collector_assignment(vars) do
+    nothing = Enum.map(vars, fn _ -> :nothing end)
+    cleaned_vars =
+      clean_vars(vars)
+      |> Enum.reduce(nothing, fn({var, index}, acc) ->
+      List.replace_at(acc, index, var)
+    end)
+
+    vars_asts = for var <- cleaned_vars, do: Macro.var(var, nil)
+    ast = {:{}, [], vars_asts}
+    quote do
+      unquote(ast) <- Task.await(collector, 30_000)
+    end
+  end
+
+  defp collector_ast(vars) do
+    quote do
+      collector = Task.async fn ->
+        IO.puts "running"
+        loop = fn(state, fun) ->
+          new_state = receive(do: unquote(receive_lines(vars)))
+          case new_state do
+            unquote(state_match_line(vars))
+          end
+        end
+        loop.(unquote(Macro.escape nothing_tuple(vars)), loop)
+      end
+    end
+  end
+
+  defp collector_sends(lefts) do
+    Enum.map lefts, fn({var_name, var_index}) ->
+      quote do
+        send(collector.pid, {unquote(var_index), unquote(Macro.var(var_name, nil))})
+      end
+    end
+  end
+
+  defp do_async({:with, _, withs}, block) do
     weths = for weth <- withs, do: do_withs(weth)
     weths = Enum.reject(weths, &(&1 == []))
 
@@ -41,156 +95,15 @@ defmodule AsyncWith do
     end
   end
 
-  defp tasks_ast(weths = [{_lefts, _rights, _expr}|_rest]) do
-    tasks_tuple = 1..(length(weths)) |> Enum.to_list |> List.to_tuple
-
-    asts = for {{lefts, rights, expr}, index} <- Enum.with_index(weths) do
-      sends = Enum.map(Enum.with_index(weths),
-        fn({{_lefts, rights, _}, index}) ->
-          {rights -- (rights -- lefts), index}
-        end)
-        |> Enum.filter(fn({deps, _index}) -> deps != [] end)
-        |> Enum.flat_map(fn({deps, task_index}) ->
-          Enum.map(deps, fn({var_name, var_index}) ->
-            {var_index, var_name, task_index}
-          end)
-        end)
-
-      task_ast(index, rights, expr, sends, lefts)
-    end
-
-    quote do
-      tasks = unquote(Macro.escape tasks_tuple)
-      unquote_splicing(asts)
-      for task <- Tuple.to_list(tasks) do
-        send(task |> elem(1), {:tasks, tasks})
-      end
-    end
+  defp do_which_vars(ast) do
+    {_, acc} = Macro.prewalk(ast, [], fn x, acc -> {x, which_vars(x) ++ acc} end)
+    acc
   end
 
-  def task_ast(index, right_vars, clause, sends, left_vars) do
-    quote do
-      tasks = put_elem(tasks, unquote(index), Task.start fn ->
-        tasks = receive do
-          {:tasks, tasks} -> tasks
-        end
-
-        loop = fn(state, fun) ->
-          new_state = unquote(wait_receive(right_vars))
-          case new_state do
-            unquote(state_match_line(right_vars))
-          end
-        end
-
-        return = loop.(unquote(Macro.escape expanding_nothing_tuple(right_vars)), loop)
-
-        with unquote_splicing(right_vars_with_clauses(right_vars)),
-             unquote(clause) do
-          unquote(task_sends(sends))
-          unquote(collector_sends(left_vars))
-        else
-          val ->
-            send(collector.pid, {:abort, val})
-            Task.start fn ->
-              unquote(tasks_send_abort())
-            end
-        end
-      end)
-    end
+  defp do_withs(expr = {:<-, _, [left, right]}) do
+    {do_which_vars(left), do_which_vars(right), expr}
   end
-
-  def right_vars_with_clauses(rights) do
-    for {name, index} <- rights do
-      quote do
-        unquote(Macro.var(name, nil)) = elem(return, unquote(index))
-      end
-    end
-  end
-
-  def wait_receive(vars) do
-    case vars do
-      [] ->
-        quote do
-          state
-        end
-      [_|_] ->
-        quote do
-          receive(do: unquote(receive_lines(vars)))
-        end
-    end
-  end
-
-  def collector_sends(lefts) do
-    Enum.map lefts, fn({var_name, var_index}) ->
-      quote do
-        send(collector.pid, {unquote(var_index), unquote(Macro.var(var_name, nil))})
-      end
-    end
-  end
-
-  def tasks_send_abort do
-    quote do
-      Enum.map Tuple.to_list(tasks), fn(task) ->
-        elem(task, 1) |> Process.exit(:with_abort)
-      end
-    end
-  end
-
-  def task_sends(sends) do
-    Enum.map sends, fn({var_index, var_name, task_index}) ->
-      quote do
-        send(elem(tasks, unquote(task_index)) |> elem(1),
-          {unquote(var_index), unquote(Macro.var(var_name, nil))})
-      end
-    end
-  end
-
-  def collector_ast(vars) do
-    quote do
-      collector = Task.async fn ->
-        IO.puts "running"
-        loop = fn(state, fun) ->
-          new_state = receive(do: unquote(receive_lines(vars)))
-          case new_state do
-            unquote(state_match_line(vars))
-          end
-        end
-        loop.(unquote(Macro.escape nothing_tuple(vars)), loop)
-      end
-    end
-  end
-
-  defp clean_vars(vars, replace \\ nil) do
-    IO.puts "clean_vars"
-    vars
-    |> Enum.map_reduce([], fn(var = {name, index}, acc) ->
-      case name in acc do
-        true -> {replace || {:_, index}, acc}
-        false -> {var, [name|acc]}
-      end
-    end)
-    |> elem(0)
-  end
-
-  def collector_assignment(vars) do
-    nothing = Enum.map(vars, fn _ -> :nothing end)
-    cleaned_vars =
-      clean_vars(vars)
-      |> Enum.reduce(nothing, fn({var, index}, acc) ->
-      List.replace_at(acc, index, var)
-    end)
-
-    vars_asts = for var <- cleaned_vars, do: Macro.var(var, nil)
-    ast = {:{}, [], vars_asts}
-    quote do
-      unquote(ast) <- Task.await(collector, 30_000)
-    end
-  end
-
-  defp nothing_tuple(vars) do
-    Enum.map(vars, fn _ -> :nothing end)
-    |> List.to_tuple
-  end
+  defp do_withs(otherwise), do: []
 
   defp expanding_nothing_tuple(vars) do
     case vars do
@@ -208,6 +121,33 @@ defmodule AsyncWith do
           List.replace_at(acc, index, :nothing)
         end)
         |> List.to_tuple
+    end
+  end
+
+  defp nothing_tuple(vars) do
+    Enum.map(vars, fn _ -> :nothing end)
+    |> List.to_tuple
+  end
+
+  defp receive_lines(vars) do
+    clauses = Enum.flat_map(vars, fn({_var, index}) ->
+      quote do
+        {unquote(index), val} -> put_elem(state, unquote(index), {:ok, val})
+      end
+    end)
+
+    abort = quote do
+      return = {:abort, _return} -> return
+    end
+
+    abort ++ clauses
+  end
+
+  defp right_vars_with_clauses(rights) do
+    for {name, index} <- rights do
+      quote do
+        unquote(Macro.var(name, nil)) = elem(return, unquote(index))
+      end
     end
   end
 
@@ -246,28 +186,92 @@ defmodule AsyncWith do
     end
   end
 
-  defp receive_lines(vars) do
-    clauses = Enum.flat_map(vars, fn({_var, index}) ->
-      quote do
-        {unquote(index), val} -> put_elem(state, unquote(index), {:ok, val})
-      end
-    end)
+  defp task_ast(index, right_vars, clause, sends, left_vars) do
+    quote do
+      tasks = put_elem(tasks, unquote(index), Task.start fn ->
+        tasks = receive do
+          {:tasks, tasks} -> tasks
+        end
 
-    abort = quote do
-      return = {:abort, _return} -> return
+        loop = fn(state, fun) ->
+          new_state = unquote(wait_receive(right_vars))
+          case new_state do
+            unquote(state_match_line(right_vars))
+          end
+        end
+
+        return = loop.(unquote(Macro.escape expanding_nothing_tuple(right_vars)), loop)
+
+        with unquote_splicing(right_vars_with_clauses(right_vars)),
+             unquote(clause) do
+          unquote(task_sends(sends))
+          unquote(collector_sends(left_vars))
+        else
+          val ->
+            send(collector.pid, {:abort, val})
+            Task.start fn ->
+              unquote(tasks_send_abort())
+            end
+        end
+      end)
+    end
+  end
+
+  defp task_sends(sends) do
+    Enum.map sends, fn({var_index, var_name, task_index}) ->
+      quote do
+        send(elem(tasks, unquote(task_index)) |> elem(1),
+          {unquote(var_index), unquote(Macro.var(var_name, nil))})
+      end
+    end
+  end
+
+  defp tasks_ast(weths = [{_lefts, _rights, _expr}|_rest]) do
+    tasks_tuple = 1..(length(weths)) |> Enum.to_list |> List.to_tuple
+
+    asts = for {{lefts, rights, expr}, index} <- Enum.with_index(weths) do
+      sends = Enum.map(Enum.with_index(weths),
+        fn({{_lefts, rights, _}, index}) ->
+          {rights -- (rights -- lefts), index}
+        end)
+        |> Enum.filter(fn({deps, _index}) -> deps != [] end)
+        |> Enum.flat_map(fn({deps, task_index}) ->
+          Enum.map(deps, fn({var_name, var_index}) ->
+            {var_index, var_name, task_index}
+          end)
+        end)
+
+      task_ast(index, rights, expr, sends, lefts)
     end
 
-    abort ++ clauses
+    quote do
+      tasks = unquote(Macro.escape tasks_tuple)
+      unquote_splicing(asts)
+      for task <- Tuple.to_list(tasks) do
+        send(task |> elem(1), {:tasks, tasks})
+      end
+    end
   end
 
-  defp do_withs(expr = {:<-, _, [left, right]}) do
-    {do_which_vars(left), do_which_vars(right), expr}
+  defp tasks_send_abort do
+    quote do
+      Enum.map Tuple.to_list(tasks), fn(task) ->
+        elem(task, 1) |> Process.exit(:with_abort)
+      end
+    end
   end
-  defp do_withs(otherwise), do: []
 
-  defp do_which_vars(ast) do
-    {_, acc} = Macro.prewalk(ast, [], fn x, acc -> {x, which_vars(x) ++ acc} end)
-    acc
+  defp wait_receive(vars) do
+    case vars do
+      [] ->
+        quote do
+          state
+        end
+      [_|_] ->
+        quote do
+          receive(do: unquote(receive_lines(vars)))
+        end
+    end
   end
 
   defp which_vars({name, meta, context}) when is_atom(context) do
