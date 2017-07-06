c0d7e8ae1fc7fd47ccc51b80344accc4762f0c60
Luke Imhoff <Kronic.Deth@gmail.com>
2017-07-05 22:34:59 -0500

    Format pipelines

--- lib/async_with.ex
+++ lib/async_with.ex
@@ -9,25 +9,33 @@ defmodule AsyncWith do
 
   defp clean_vars(vars, replace \\ nil) do
     vars
-    |> Enum.map_reduce([], fn(var = {name, index}, acc) ->
-      case name in acc do
-        true -> {replace || {:_, index}, acc}
-        false -> {var, [name|acc]}
-      end
-    end)
+    |> Enum.map_reduce(
+         [],
+         fn (var = {name, index}, acc) ->
+           case name in acc do
+             true -> {replace || {:_, index}, acc}
+             false -> {var, [name|acc]}
+           end
+         end
+       )
     |> elem(0)
   end
 
   defp collector_assignment(vars) do
     nothing = Enum.map(vars, fn _ -> :nothing end)
     cleaned_vars =
-      clean_vars(vars)
-      |> Enum.reduce(nothing, fn({var, index}, acc) ->
-      List.replace_at(acc, index, var)
-    end)
+      vars
+      |> clean_vars()
+      |> Enum.reduce(
+           nothing,
+           fn ({var, index}, acc) ->
+             List.replace_at(acc, index, var)
+           end
+         )
 
     vars_asts = for var <- cleaned_vars, do: Macro.var(var, nil)
     ast = {:{}, [], vars_asts}
+
     quote do
       unquote(ast) <- Task.await(collector, 30_000)
     end
@@ -36,19 +44,21 @@ defmodule AsyncWith do
   defp collector_ast(vars) do
     quote do
       collector = Task.async fn ->
-        loop = fn(state, fun) ->
+        loop = fn (state, fun) ->
           new_state = receive(do: unquote(receive_lines(vars)))
+
           case new_state do
             unquote(state_match_line(vars))
           end
         end
+
         loop.(unquote(Macro.escape nothing_tuple(vars)), loop)
       end
     end
   end
 
   defp collector_sends(lefts) do
-    Enum.map lefts, fn({var_name, var_index}) ->
+    Enum.map lefts, fn ({var_name, var_index}) ->
       quote do
         send(collector.pid, {unquote(var_index), unquote(Macro.var(var_name, nil))})
       end
@@ -59,25 +69,27 @@ defmodule AsyncWith do
     weths = for weth <- withs, do: do_withs(weth)
     weths = Enum.reject(weths, &(&1 == []))
 
-    {weths, _lefts} =
-      Enum.map_reduce(weths, [], fn(weth, acc) ->
-        {lefts, rights, expr} = weth
-        rights = Enum.uniq(rights)
-        rights = Enum.filter(rights, &(&1 in acc))
-        {{lefts, rights, expr}, lefts ++ acc}
-      end)
+    {weths, _lefts} = Enum.map_reduce weths, [], fn (weth, acc) ->
+      {lefts, rights, expr} = weth
+      rights = Enum.uniq(rights)
+      rights = Enum.filter(rights, &(&1 in acc))
 
-    {weths, {_, vars}} =
-      Enum.map_reduce(weths, {-1, []}, fn(weth, acc) ->
-        {lefts, rights, expr} = weth
-        {index, current_lefts} = acc
-        rights = Enum.map(rights, fn(x) -> {x, Keyword.get(current_lefts, x)} end)
-        {lefts, index} = Enum.map_reduce(lefts, index, &({{&1, &2 + 1}, &2 + 1}))
-        current_lefts = lefts ++ current_lefts
-        {{lefts, rights, expr}, {index, current_lefts}}
-      end)
+      {{lefts, rights, expr}, lefts ++ acc}
+    end
+
+    {weths, {_, vars}} = Enum.map_reduce weths, {-1, []}, fn(weth, acc) ->
+      {lefts, rights, expr} = weth
+      {index, current_lefts} = acc
+      rights = Enum.map(rights, fn(x) -> {x, Keyword.get(current_lefts, x)} end)
+      {lefts, index} = Enum.map_reduce(lefts, index, &({{&1, &2 + 1}, &2 + 1}))
+      current_lefts = lefts ++ current_lefts
+
+      {{lefts, rights, expr}, {index, current_lefts}}
+    end
+
     col_ast = collector_ast(vars)
     t_ast = tasks_ast(weths)
+
     quote do
       unquote(col_ast)
       unquote(t_ast)
@@ -102,32 +114,37 @@ defmodule AsyncWith do
     case vars do
       [] -> {}
       vars ->
-        highest =
-          Enum.max_by(vars, fn({_, index}) -> index end)
-          |> elem(1)
+        highest = vars
+                  |> Enum.max_by(fn({_, index}) -> index end)
+                  |> elem(1)
 
         nothings = 0..highest
-        |> Enum.to_list
-        |> Enum.map(fn _ -> {:ok, :nothing} end)
+                   |> Enum.to_list()
+                   |> Enum.map(fn _ -> {:ok, :nothing} end)
 
-        Enum.reduce(vars, nothings, fn({var, index}, acc) ->
-          List.replace_at(acc, index, :nothing)
-        end)
-        |> List.to_tuple
+        vars
+        |> Enum.reduce(
+             nothings,
+             fn ({var, index}, acc) ->
+               List.replace_at(acc, index, :nothing)
+             end
+           )
+        |> List.to_tuple()
     end
   end
 
   defp nothing_tuple(vars) do
-    Enum.map(vars, fn _ -> :nothing end)
-    |> List.to_tuple
+    vars
+    |> Enum.map(fn _ -> :nothing end)
+    |> List.to_tuple()
   end
 
   defp receive_lines(vars) do
-    clauses = Enum.flat_map(vars, fn({_var, index}) ->
+    clauses = Enum.flat_map vars, fn ({_var, index}) ->
       quote do
         {unquote(index), val} -> put_elem(state, unquote(index), {:ok, val})
       end
-    end)
+    end
 
     abort = quote do
       return = {:abort, _return} -> return
@@ -148,25 +165,34 @@ defmodule AsyncWith do
     nothing = case vars do
       [] -> []
       vars ->
-        highest = Enum.max_by(vars, fn({_, index}) -> index end) |> elem(1)
-        Enum.map(0..highest |> Enum.to_list, fn _ -> :nothing end)
+        highest = vars
+                  |> Enum.max_by(fn({_, index}) -> index end)
+                  |> elem(1)
+        Enum.map(0..highest |> Enum.to_list(), fn _ -> :nothing end)
     end
-    new_vars =
-      clean_vars(vars, {:nothing, -1})
-    |> Enum.reduce(nothing, fn({var, index}, acc) ->
-      case (-1 == index) do
-        true -> acc
-        false -> List.replace_at(acc, index, var)
-      end
-    end)
+
+    new_vars = vars
+               |> clean_vars({:nothing, -1})
+               |> Enum.reduce(
+                    nothing,
+                    fn ({var, index}, acc) ->
+                      case (-1 == index) do
+                        true -> acc
+                        false -> List.replace_at(acc, index, var)
+                      end
+                    end
+                  )
 
     vars_ast = for var <- new_vars, do: var == :nothing || Macro.var(var, nil)
 
-    cleaned_vars =
-      clean_vars(vars)
-      |> Enum.reduce(nothing, fn({var, index}, acc) ->
-      List.replace_at(acc, index, var)
-    end)
+    cleaned_vars = vars
+                   |> clean_vars()
+                   |> Enum.reduce(
+                        nothing,
+                        fn ({var, index}, acc) ->
+                          List.replace_at(acc, index, var)
+                        end
+                      )
 
     oks_vars = for var <- cleaned_vars, do: {:ok, Macro.var(var, nil)}
     oks = {:{}, [], oks_vars}
@@ -181,13 +207,14 @@ defmodule AsyncWith do
 
   defp task_ast(index, right_vars, clause, sends, left_vars) do
     quote do
-      tasks = put_elem(tasks, unquote(index), Task.start fn ->
+      tasks = put_elem tasks, unquote(index), Task.start fn ->
         tasks = receive do
           {:tasks, tasks} -> tasks
         end
 
         loop = fn(state, fun) ->
           new_state = unquote(wait_receive(right_vars))
+
           case new_state do
             unquote(state_match_line(right_vars))
           end
@@ -202,16 +229,17 @@ defmodule AsyncWith do
         else
           val ->
             send(collector.pid, {:abort, val})
+
             Task.start fn ->
               unquote(tasks_send_abort())
             end
         end
-      end)
+      end
     end
   end
 
   defp task_sends(sends) do
-    Enum.map sends, fn({var_index, var_name, task_index}) ->
+    Enum.map sends, fn ({var_index, var_name, task_index}) ->
       quote do
         send(elem(tasks, unquote(task_index)) |> elem(1),
           {unquote(var_index), unquote(Macro.var(var_name, nil))})
@@ -220,19 +248,26 @@ defmodule AsyncWith do
   end
 
   defp tasks_ast(weths = [{_lefts, _rights, _expr}|_rest]) do
-    tasks_tuple = 1..(length(weths)) |> Enum.to_list |> List.to_tuple
+    tasks_tuple = 1..(length(weths))
+                  |> Enum.to_list()
+                  |> List.to_tuple()
 
     asts = for {{lefts, rights, expr}, index} <- Enum.with_index(weths) do
-      sends = Enum.map(Enum.with_index(weths),
-        fn({{_lefts, rights, _}, index}) ->
-          {rights -- (rights -- lefts), index}
-        end)
-        |> Enum.filter(fn({deps, _index}) -> deps != [] end)
-        |> Enum.flat_map(fn({deps, task_index}) ->
-          Enum.map(deps, fn({var_name, var_index}) ->
-            {var_index, var_name, task_index}
-          end)
-        end)
+      sends = weths
+              |> Enum.with_index()
+              |> Enum.map(
+                   fn ({{_lefts, rights, _}, index}) ->
+                     {rights -- (rights -- lefts), index}
+                   end
+                 )
+              |> Enum.filter(fn ({deps, _index}) -> deps != [] end)
+              |> Enum.flat_map(
+                   fn ({deps, task_index}) ->
+                     Enum.map deps, fn ({var_name, var_index}) ->
+                       {var_index, var_name, task_index}
+                     end
+                   end
+                 )
 
       task_ast(index, rights, expr, sends, lefts)
     end
@@ -240,6 +275,7 @@ defmodule AsyncWith do
     quote do
       tasks = unquote(Macro.escape tasks_tuple)
       unquote_splicing(asts)
+
       for task <- Tuple.to_list(tasks) do
         send(task |> elem(1), {:tasks, tasks})
       end
@@ -249,7 +285,9 @@ defmodule AsyncWith do
   defp tasks_send_abort do
     quote do
       Enum.map Tuple.to_list(tasks), fn(task) ->
-        elem(task, 1) |> Process.exit(:with_abort)
+        task
+        |> elem(1)
+        |> Process.exit(:with_abort)
       end
     end
   end
